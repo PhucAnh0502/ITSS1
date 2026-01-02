226e4bf0aed0a43f53c4ac7d85f624cdc465c63c
bachhungcb <bachhungcb@gmail.com>
2026-01-02 23:35:00 +0700

    feat: add build option for vite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Nếu file nằm trong node_modules, tách nó ra thành file vendor riêng
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
